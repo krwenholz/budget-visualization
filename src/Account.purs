0fa7c49dc656dcd6130128f4ddf78b8b16bb93e3
Kyle R Wenholz <kyle@krwenholz.com>
2016-11-29 18:41:13 -0800

    Add a percentChange field to income events

--- src/Account.purs
+++ src/Account.purs
@@ -10,26 +10,26 @@ import Data.Either (either)
 import Global (isNaN, readFloat)
 
 data Action
-  = UpdateAccount { name :: String
+  = UpdateAccount { name :: String -- TODO: Update could be an either or a few types to be cleaner
                   , initialValue :: Number }
-  |UpdateIncomeEvent { eventNum :: Int
-                     , name :: String
-                     , change :: Number }
+  | UpdateIncomeEvent { eventNum :: Int
+                      , event :: IncomeEvent }
   | DeleteIncomeEvent Int
   | NewIncomeEvent
 
 type IncomeEvent = { name :: String
-                   , change :: Number }
+                   , flatChange :: Number
+                   , percentChange :: Number }
 
 updateIncomeEvent :: Int -> IncomeEvent -> Action
-updateIncomeEvent eventNum { name: name, change: change } =
+updateIncomeEvent eventNum incomeEvent =
   UpdateIncomeEvent { eventNum: eventNum
-                    , name: name
-                    , change: change }
+                    , event: incomeEvent }
 
 emptyIncomeEvent :: IncomeEvent
 emptyIncomeEvent = { name: ""
-                  , change: 0.0 }
+                   , flatChange: 0.0
+                   , percentChange: 0.0 }
 
 type State = { name :: String
              , initialValue :: Number
@@ -45,11 +45,9 @@ update NewIncomeEvent account =
   account { incomeEvents = account.incomeEvents `snoc` emptyIncomeEvent }
 update (UpdateAccount { name: name, initialValue: initialValue }) account =
   account { name = name, initialValue = initialValue }
-update (UpdateIncomeEvent { eventNum: eventNum, name: name, change: change}) account =
+update (UpdateIncomeEvent { eventNum: eventNum , event: newIncomeEvent }) account =
   account { incomeEvents = (fromMaybe account.incomeEvents
-                                      (modifyAt eventNum (\_ -> newIncomeEvent) account.incomeEvents)) }
-  where
-    newIncomeEvent = { name: name, change: change}
+          (modifyAt eventNum (const newIncomeEvent) account.incomeEvents)) }
 update (DeleteIncomeEvent eventNum) account =
   account { incomeEvents = (fromMaybe account.incomeEvents
                                       (deleteAt eventNum account.incomeEvents)) }
@@ -66,12 +64,15 @@ incomeEventInput incomeEvent =
     []
     [ input [ type_ "text"
             , placeholder incomeEvent.name
-            , onInput (\newName -> { name: newName.target.value, change: incomeEvent.change }) ]
+            , onInput (\newName -> incomeEvent { name = newName.target.value }) ]
             []
     , input [ type_ "number"
-            , placeholder $ show incomeEvent.change
-            , onInput (\change -> { name: incomeEvent.name
-                                  , change: (readNumber change.target.value) }) ]
+            , placeholder $ show incomeEvent.flatChange
+            , onInput (\change -> incomeEvent { flatChange = readNumber change.target.value }) ]
+            []
+    , input [ type_ "number"
+            , placeholder $ show incomeEvent.percentChange
+            , onInput (\change -> incomeEvent { percentChange = readNumber change.target.value }) ]
             []
     ]
 
